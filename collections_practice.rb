55f6e97931da3b0ed532dc281970e3c4197d609d
essential-robot-4364 <brandon.james414@gmail.com>
2019-10-28 14:39:50 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,18 @@
+def sort_array_asc(love)
+  love.sort
+end
+sort_array_asc([23,56,87,2])
 
+def sort_array_desc(heart)
+  heart.sort do |left, right|
+    right <=> left
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |left, right|
+    left.length <=> right.length
+  end
+end
+
+sort_array_char_count(["dogs", "cat", "Horses"])
